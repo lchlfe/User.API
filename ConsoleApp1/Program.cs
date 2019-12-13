0485cc336ed9c140ebc2f7e39becfcd5d6eee0d4
lchlfe <lchlfe@hotmail.com>
2019-12-13 16:58:25 +0800

    笔记

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World From Docker");
+            System.Threading.Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
